c09fea58c7de22bcc4c5e38399d486a93d6d81ba
Hoang Nguyen 7 <hoang.nguyen7@sotatek.com>
2023-03-29 23:21:50 +0700

    logout funcion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from "react";
 import "./App.css";
 import { SelectWalet, SendWalet, SignWalet } from "react-wallet-selector";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-
 	return (
-		<div>
-			<div className="container-fluid">
-				<div className="row mt-5">
-					<div className="col-4">
-						<SelectWalet />
-					</div>
-					<div className="col-4">
-						<SignWalet />
-					</div>
-					<div className="col-4">
-						<SendWalet />
-					</div>
+		<div className="container-fluid">
+			<div className="row mt-5">
+				<div className="col-4">
+					<SelectWalet />
+				</div>
+				<div className="col-4">
+					<SignWalet />
+				</div>
+				<div className="col-4">
+					<SendWalet />
 				</div>
 			</div>
 		</div>
